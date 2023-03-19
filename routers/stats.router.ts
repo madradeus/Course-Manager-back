b1bf7e61b237d6783cb81dd16675ec7f889e2794
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-03-19 22:00:11 +0100

    Added stats routing

---
+++ routers/stats.router.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { Stats } from "../services/Stats";
+
+export const statsRouter = Router();
+
+
+statsRouter
+    .get('/active-courses', async (req, res) => {
+        res.json(await Stats.getNumberOfActiveCourses());
+    })
+    .get('/active-students', async (req, res) => {
+        res.json(await Stats.getActiveStudentsNumber())
+    })
+    .get('/avg-frequency', async (req, res) => {
+        res.json(await Stats.getTotalAvgFrequency())
+    })
