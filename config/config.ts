bf7a862211986a6d01cf627bfbba09cbe496d8d2
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-04-02 21:35:30 +0200

    Added authenticate middleware

--- config/config.ts
+++ config/config.ts
@@ -4,4 +4,6 @@ export const config = {
     password: 'root',
     database: 'dance_studio',
     corsOrigin: 'http://localhost:3000',
+    tokenSecret: '9ef73bcc9fd1a0fb5e43f5dcc994c1990bd7a7ff6a314518adcb723814201ae3db0452182dd199fc20c4d47d7bd86ac17f6df9531b9e4c8d7d4783eb158a5203',
+    domain: 'localhost'
 };
